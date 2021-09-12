5e9b17f6a000e8edcf5d1a08c78357bf745abac4
Pierre-Clément Kerneïs <pc.kerneis@gmail.com>
2021-09-12 22:44:34 +0200

    Web demo page : add shortcuts for start/stop and update

--- src/web/index.ts
+++ src/web/index.ts
@@ -7,46 +7,60 @@ const OUTPUT_STORAGE_KEY = '_WEB_PLAYER_MIDI_OUTPUT_987654321'
 
 let midiOutput: any = null;
 
-(navigator as any).requestMIDIAccess().then((midiAccess: any) => {
-  const midiOutputSelect = document.getElementById('midiOutput');
-  const option = midiOutputSelect.appendChild(document.createElement('option'));
-  option.innerText = 'None';
+function buildMidiOutputOptions(midiOutputSelect: any, midiAccess: any): void {
+  const noneOption = midiOutputSelect.appendChild(document.createElement('option'));
+  noneOption.innerText = 'None';
 
   midiAccess.outputs.forEach((output: any) => {
     const option = midiOutputSelect.appendChild(document.createElement('option'));
     option.innerText = output.name;
     option.value = output.id;
   });
+}
 
-  const textArea = document.getElementById('editor');
-
+function restoreFromLocaleStorage(textArea: any, midiOutputSelect: any, midiAccess: any): void {
   const storedProgram = localStorage.getItem(PROGRAM_STORAGE_KEY);
 
   if (storedProgram) {
-    textArea['value'] = storedProgram;
+    textArea.value = storedProgram;
   }
 
   const storedOutput = localStorage.getItem(OUTPUT_STORAGE_KEY);
 
   if (storedOutput) {
-    midiOutputSelect['value'] = storedOutput;
-    midiOutput = new WebMidiOutput(midiAccess.outputs.get(midiOutputSelect['value']));
-    console.debug(midiOutput);
+    midiOutputSelect.value = storedOutput;
+    midiOutput = new WebMidiOutput(midiAccess.outputs.get(midiOutputSelect.value));
   }
+}
 
-  document.getElementById('testMidiButton').onclick = () => {
-    sendMiddleC(midiAccess, midiOutputSelect['value']);
+function updateCode(codeProvider: { code: any }, textArea: any, updateButton: any): void {
+  localStorage.setItem(PROGRAM_STORAGE_KEY, textArea.value);
+  codeProvider.code = textArea.value;
+  updateButton.disabled = true;
+}
+
+(navigator as any).requestMIDIAccess().then((midiAccess: any) => {
+  const startButton = document.getElementById('startButton') as any;
+  const updateButton = document.getElementById('updateButton') as any;
+  const midiOutputSelect = document.getElementById('midiOutput') as any;
+  const textArea = document.getElementById('editor') as any;
+  const testMidiButton = document.getElementById('testMidiButton');
+
+  let player: Player;
+  let codeProvider = {code: textArea.value};
+
+  buildMidiOutputOptions(midiOutputSelect, midiAccess);
+  restoreFromLocaleStorage(textArea, midiOutputSelect, midiAccess);
+
+  testMidiButton.onclick = () => {
+    sendMiddleC(midiAccess, midiOutputSelect.value);
   };
 
   midiOutputSelect.onchange = () => {
-    midiOutput = new WebMidiOutput(midiAccess.outputs.get(midiOutputSelect['value']));
-    console.debug(midiOutput);
+    midiOutput = new WebMidiOutput(midiAccess.outputs.get(midiOutputSelect.value));
   };
 
-  const startButton = document.getElementById('startButton');
-  let player: Player;
-
-  startButton.onclick = () => {
+  function togglePlayState(): void {
     if (player != null) {
       startButton.innerText = 'Start';
       player.stop();
@@ -54,7 +68,7 @@ let midiOutput: any = null;
     } else {
       startButton.innerText = 'Stop';
       player = Player.create({
-        codeProvider: {code: textArea['value']},
+        codeProvider,
         clockFn: () => performance.now() / 1000,
         processors: [new MidiProcessor(midiOutput)],
         entryPoint: 'Root'
@@ -62,14 +76,28 @@ let midiOutput: any = null;
 
       player.start('Root');
     }
-  };
+  }
 
-  textArea.oninput = debounce(() => {
-    localStorage.setItem(PROGRAM_STORAGE_KEY, textArea['value']);
-  });
+  startButton.onclick = () => togglePlayState();
+
+  textArea.oninput = () => updateButton.disabled = false;
 
   midiOutputSelect.oninput = debounce(() => {
-    localStorage.setItem(OUTPUT_STORAGE_KEY, midiOutputSelect['value']);
+    localStorage.setItem(OUTPUT_STORAGE_KEY, midiOutputSelect.value);
+  });
+
+  updateButton.onclick = () => updateCode(codeProvider, textArea, updateButton);
+
+  window.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && event.code === 'KeyS') {
+      updateCode(codeProvider, textArea, updateButton);
+      event.preventDefault();
+    }
+
+    if (event.ctrlKey && event.code === 'Space') {
+      togglePlayState();
+      event.preventDefault();
+    }
   });
 });
 
